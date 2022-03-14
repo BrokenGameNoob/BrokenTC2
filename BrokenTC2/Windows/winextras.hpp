ee92443bcbb573a60ea3f222b4c76618d4ef0ae0
BrokenGameNoob <brokenGameNoob@gmail.com>
2022-03-14 13:14:19 +0200

    added license header files

--- BrokenTC2/Windows/winextras.hpp
+++ BrokenTC2/Windows/winextras.hpp
@@ -1,10 +1,24 @@
+/* reuse of antimicro Gamepad to KB+M event mapper https://github.com/AntiMicroX/antimicrox
+ * Copyright (C) 2015 Travis Nickles <nickles.travis@gmail.com>
+ * Copyright (C) 2020 Jagoda Górska <juliagoda.pl@protonmail>
+ * Copyright (C) 2022 BrokenGameNoob <brokengamenoob@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef WINDAUBE_WINEXTRAS_H
 #define WINDAUBE_WINEXTRAS_H
-
-/*!
- *  This file is based on the antimicroX project
- *  https://github.com/AntiMicroX/antimicrox
-*/
 
 #include <QHash>
 #include <QObject>
